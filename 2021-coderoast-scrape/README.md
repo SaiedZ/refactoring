0865ec8b45ea05fe5f56c2b67f2c3dbbc7e81145
Deias <saied_am@hotmail.com>
2023-02-11 17:43:14 +0100

    creating readme file

--- 2021-coderoast-scrape/README.md
+++ 2021-coderoast-scrape/README.md
@@ -1,6 +1,29 @@
-# Web and PDF Scraper Refactoring
+# Web and PDF Scraper
+
+This module looks up bibliographic information from directories, csv files and pandas databases.
+
+From a .csv file input, you can get:
+    - a folder of .pdf downloads (experimental).
+    - a pandas dataframe
+    - another .csv file
+
+From a pandas dataframe file input, you can get:
+    - a folder of .pdf downloads (experimental).
+    - another pandas dataframe, but the citations of the input are each returned with their own citation information.
+
+From a folder input, you can get:
+    - a pandas dataframe
+    - a .csv file
+
+## Web and PDF Scraper Refactoring
 
 This repository contains the example code of the Web and PDF scraper code roast. Here are the links to the videos:
 
 - Part 1: https://youtu.be/MXM6VEtf8SE
 - Part 2: https://youtu.be/6ac4Um2Vicg
+
+
+# License
+
+The MIT License [https://opensource.org/licenses/MIT]
+Copyright (c) 2021 John Fallot
--- 2021-coderoast-scrape/after/main.py
+++ 2021-coderoast-scrape/after/main.py
@@ -1,37 +1,3 @@
-r"""This module looks up bibliographic information from directories, csv files and pandas databases.
-
-From a .csv file input, you can get:
-    - a folder of .pdf downloads (experimental).
-    - a pandas dataframe
-    - another .csv file
-
-From a pandas dataframe file input, you can get:
-    - a folder of .pdf downloads (experimental).
-    - another pandas dataframe, but the citations of the input are each returned with their own citation information.
-
-From a folder input, you can get:
-    - a pandas dataframe
-    - a .csv file
-"""
-
-# ==============================================
-#    SPECIAL THANKS
-#
-#    Michele Cotrufo
-#    Nathan Lippi
-#    Jon Watson Rooney
-#    Colin Meret
-#    ArjanCodes
-#    James Murphy
-#    Micael Jarniac
-#
-#    ----------------Maintainer----------------
-#    John Fallot
-#
-#    ----------------License----------------
-#    The MIT License [https://opensource.org/licenses/MIT]
-#    Copyright (c) 2021 John Fallot
-# ==============================================
 
 # ==============================================
 #      IMPORTS
